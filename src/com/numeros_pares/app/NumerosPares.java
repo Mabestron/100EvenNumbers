4a7d53595d3e65fdab3790938189eaa9819ced77
Mabestron <ivangamaliel555@hotmail.com>
2019-11-15 23:57:03 -0600

    Even Numbers

---
+++ src/com/numeros_pares/app/NumerosPares.java
@@ -0,0 +1,16 @@
+package com.numeros_pares.app;
+
+public class NumerosPares 
+{
+	public static void main(String[]args)
+	{
+		for(int i=1; i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
